b1488f700487005ebf9cb95439b8cb42dc5e602a
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-29 14:18:18 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,26 @@
+  vector<int>rank,parent;
+    DSU(int n){
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+    int find(int x){
+        if(x==parent[x])return x;
+        return parent[x]= find(parent[x]);
+    }
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+        if(x_parent==y_parent)return ;
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }else {
+            parent[y_parent]=x_parent;
+            rank[x_parent]++;
+        }
+    }
