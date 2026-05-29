f338498499d2e5eb689bfbdef5f23b7c2d0a71a7
Divyanshu Rathore <dnd.mein@gmail.com>
2026-05-29 15:37:53 +0530

    Update Accounts Merge.cpp

--- problems/Accounts Merge/Accounts Merge.cpp
+++ problems/Accounts Merge/Accounts Merge.cpp
@@ -1,41 +1,71 @@
-class DSU{
-    public:
-    vector<int>rank,parent;
-    DSU(int n){
+class DSU {
+public:
+    vector<int> parent, rank;
+
+    DSU(int n) {
         parent.resize(n);
         rank.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            rank[i]=0;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
         }
     }
-    int find(int x){
-        if(x==parent[x])return x;
-        return parent[x]= find(parent[x]);
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
     }
-    void UNION(int x,int y){
-        int x_parent=find(x);
-        int y_parent=find(y);
-        if(x_parent==y_parent)return ;
-        if(rank[x_parent]>rank[y_parent]){
-            parent[y_parent]=x_parent;
-        }else if(rank[x_parent]<rank[y_parent]){
-            parent[x_parent]=y_parent;
-        }else {
-            parent[y_parent]=x_parent;
+    void UNION(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent)
+            return;
+
+        if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else {
+            parent[y_parent] = x_parent;
             rank[x_parent]++;
         }
     }
 };
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
         DSU dsu(n);
-        for(auto&e:edges){
-            int u=e[0];
-            int v=e[1];
-            dsu.UNION(u,v);
+        unordered_map<string, int> mailToIndex;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if (mailToIndex.count(mail)) {
+                    dsu.UNION(i, mailToIndex[mail]);
+                } else {
+                    mailToIndex[mail] = i;
+                }
+            }
         }
-        return dsu.find(source)==dsu.find(destination);
+        vector<vector<string>> mergedMail(n);
+        for (auto it : mailToIndex) {
+            string mail = it.first;
+            int idx = it.second;
+            int parent = dsu.find(idx);
+            mergedMail[parent].push_back(mail);
+        }
+        vector<vector<string>> ans;
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0)
+                continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for (string& mail : mergedMail[i]) {
+                temp.push_back(mail);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
